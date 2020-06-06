9a15e056ff1d48c285c3f552c6102cb3af96168f
Jordan Handy <handy.jordan@gmail.com>
2020-06-06 10:57:48 -0400

    add functionality to delete stocks

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+# if the stock is already tracked
+# create a stock variable, where we check the database for an existing ticker symbol
+# if one does not exist, we return false
+# if one does exist, we return true / false where the stock exists as a tracked stock in the user table
 def stock_already_tracked?(ticker_symbol)
   stock = Stock.check_db(ticker_symbol)
   return false unless stock
   stocks.where(id: stock.id).exists?
 end
+
+# we check if the current number of stocks associated with the user is less than 10
 def under_stock_limit?
   stocks.count < 10
 end
+
+# if they are under the stock limit, and the stock is not being tracked,
+# let them track the stock
 def can_track_stock?(ticker_symbol)
   under_stock_limit? && !stock_already_tracked?(ticker_symbol)
 end
